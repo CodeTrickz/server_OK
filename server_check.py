f93e9b6b7fc41441ec73a4978ea83f4e1cf49b37
Wesley <Wesley.vanhoof@student.ap.be>
2023-10-27 15:16:31 +0200

    updated server_check.py , ping bug fixed

--- server_check.py
+++ server_check.py
@@ -1,8 +1,10 @@
 import os
 import json
+import math
 from ping3 import ping
 
 SERVERS_FILE = "data/servers.json"
+
 
 def run_checks():
     print("server will be checked...")
@@ -15,16 +17,21 @@ def run_checks():
 
     save_servers(servers)
 
+
 def ping_server(addr):
     print(f"{addr}: {ping(addr)}")
     response = ping(addr)
-    if response == False:
-        return "Online"
-    else:
+    print(response)
+    if response < 0:
         return "Offline"
+    else:
+        return "Online"
+
 
 def update_status(server, status):
     print("status server")
+
+
 def generate_html_report():
     print("maak html pagina")
 
@@ -36,6 +43,7 @@ def load_servers():
     except (FileNotFoundError, json.JSONDecodeError):
         servers = []
     return servers
+
 
 def save_servers(servers):
     with open(SERVERS_FILE, 'w') as file:
