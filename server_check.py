b9540740626808926b536c9ea3dec070d529408f
Wesley <Wesley.vanhoof@student.ap.be>
2023-11-01 14:25:26 +0100

    created html for prev checks

--- server_check.py
+++ server_check.py
@@ -10,16 +10,24 @@ PREV_CHECKS = "data/PrevChecks.json"
 
 def run_checks():
     print("server will be checked...")
-    servers = load_servers()
+    servers = load_servers(SERVERS_FILE)
     time = datetime.now()
     timeStr = f"{time.date()}  {time.strftime('%H:%M:%S')}"
 
+    prev_checks = load_servers(PREV_CHECKS)
     for server in servers:
         addr = server["address"]
         status = ping_server(addr)
         server["status"] = status
         server["lastCheck"] = timeStr
-    save_servers(servers)
+        prev_checks.append({
+            "address": addr,
+            "name": server["name"],
+            "status": status,
+            "lastCheck": timeStr
+        })
+    save_servers(servers, SERVERS_FILE)
+    save_servers(prev_checks, PREV_CHECKS)
 
 
 def ping_server(addr):
@@ -32,7 +40,8 @@ def ping_server(addr):
 
 
 def generate_html_report():
-    servers = load_servers()
+    servers = load_servers(SERVERS_FILE)
+    prev = load_servers(PREV_CHECKS)
 
     with open("reports/server_status_report.html", "r") as html:
         template = html.read()
@@ -50,16 +59,32 @@ def generate_html_report():
     with open("reports/server_status_report.html", "w") as report_file:
         report_file.write(report)
 
+    with open("reports/server_prev_report.html", "r") as html2:
+        template2 = html2.read()
 
-def load_servers():
+    div_start2 = template2.find("<div>")
+    div_end2 = template2.find("</div>")
+    div_data2 = template2[div_start2:div_end2]
+
+    updated_div2 = "<div><h2>Server name</h2><h2>Server Host</h2><h2>Server Status</h2><h2>last Check</h2>"
+    for previous in prev:
+        updated_div2 += f"<p>{previous['name']}</p><p>{previous['address']}</p><p>{previous['status']}</p><p>{previous['lastCheck']}</p>"
+
+    report2 = template2.replace(div_data2, updated_div2)
+
+    with open("reports/server_prev_report.html", "w") as report_file2:
+        report_file2.write(report2)
+
+
+def load_servers(loadFile):
     try:
-        with open(SERVERS_FILE, 'r') as file:
+        with open(loadFile, 'r') as file:
             servers = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         servers = []
     return servers
 
 
-def save_servers(servers):
-    with open(SERVERS_FILE, 'w') as file:
+def save_servers(servers, saveFile):
+    with open(saveFile, 'w') as file:
         json.dump(servers, file)
