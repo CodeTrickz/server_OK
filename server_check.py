701f8d978f4c0cc457762ef3b935fab61090d057
Wesley <Wesley.vanhoof@student.ap.be>
2023-11-06 15:04:00 +0100

    cleaning up useless lines

--- server_check.py
+++ server_check.py
@@ -56,8 +56,12 @@ def ping_server(addr):
 def generate_html_report():
     servers = load_servers(SERVERS_FILE)
     prev = load_servers(PREV_CHECKS)
+    html_modification("reports/server_status_report.html", servers)
+    html_modification("reports/server_prev_report.html", prev)
 
-    with open("reports/server_status_report.html", "r") as html:
+
+def html_modification(path, serverList):
+    with open(path, "r") as html:
         template = html.read()
 
     div_start = template.find("<div>")
@@ -65,29 +69,13 @@ def generate_html_report():
     div_data = template[div_start:div_end]
 
     updated_div = "<div><h2>Server name</h2><h2>Server Host</h2><h2>Server Status</h2><h2>last Check</h2>"
-    for server in servers:
+    for server in serverList:
         updated_div += f"<p>{server['name']}</p><p>{server['address']}</p><p>{server['status']}</p><p>{server['lastCheck']}</p>"
 
     report = template.replace(div_data, updated_div)
 
-    with open("reports/server_status_report.html", "w") as report_file:
+    with open(path, "w") as report_file:
         report_file.write(report)
-
-    with open("reports/server_prev_report.html", "r") as html2:
-        template2 = html2.read()
-
-    div_start2 = template2.find("<div>")
-    div_end2 = template2.find("</div>")
-    div_data2 = template2[div_start2:div_end2]
-
-    updated_div2 = "<div><h2>Server name</h2><h2>Server Host</h2><h2>Server Status</h2><h2>last Check</h2>"
-    for previous in prev:
-        updated_div2 += f"<p>{previous['name']}</p><p>{previous['address']}</p><p>{previous['status']}</p><p>{previous['lastCheck']}</p>"
-
-    report2 = template2.replace(div_data2, updated_div2)
-
-    with open("reports/server_prev_report.html", "w") as report_file2:
-        report_file2.write(report2)
 
 
 def load_servers(loadFile):
